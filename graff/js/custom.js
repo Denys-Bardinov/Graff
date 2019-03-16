51e9c57839564fd60d0271f79ec2ad5657cfda6f
denblind0404 <40865458+denblind0404@users.noreply.github.com>
2019-03-16 09:08:20 +0200

    Add files via upload

---
+++ graff/js/custom.js
@@ -0,0 +1,9 @@
+jQuery(function ($) {
+$(function () {
+                $('.burger_trigger').click(function () {
+                    $('.menu-list').toggleClass('menu_opened');
+                });
+            });
+});
+
+
